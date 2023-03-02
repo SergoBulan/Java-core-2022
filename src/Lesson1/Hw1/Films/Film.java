d60bd87dcf13ef9d63d9db727e96531678f4e5d4
SergoBulan <108830039+SergoBulan@users.noreply.github.com>
2023-03-02 21:34:57 +0200

    new

---
+++ src/Lesson1/Hw1/Films/Film.java
@@ -0,0 +1,11 @@
+package Lesson1.Hw1.Films;
+
+//public class Film {
+ //   private int id;
+  //  private String name;
+   // private int age;
+   // private ArrayList
+
+   // public Film() {
+    }
+
---
+++ src/Lesson1/Hw1/Films/Main.java
@@ -0,0 +1,11 @@
+package Lesson1.Hw1.Films;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Film film1 = new Film(1,"The silence of the lamb",1991);
+        skills.add("Horror");
+        skills.add("Thriller");
+        System.out.println(film1);
+    }
+}
